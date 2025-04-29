93464f989ec9faf0449a9932ea359de689c3af09
spiroskonstantinidis <60156291+spiros-k@users.noreply.github.com>
2025-04-29 16:41:08 +0300

    erase some comments

--- script.sh
+++ script.sh
@@ -1,15 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 GITHUB_REPO="https://github.com/spiros-k/express_app.git"
 DOCKER_REPO="https://hub.docker.com/repository/docker/spiroskonstantinidis/express-app/general"
-
-# docker pull ubuntu
 
 git clone "$GITHUB_REPO"
 
 cd "express_app"
-# CANNOT FIND DOCKERFILE
-# DOES NOT git CLONE
-# HAVE TO SOMEHOW MAKE IT RUN GIT CLONE FROM INSIDE CONTAINER
 
 docker build -t "express-app" .
 
